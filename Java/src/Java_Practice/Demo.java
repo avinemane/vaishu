89df920d831657f6ea37450992c7728289579d7c
TLead@gmail.com <SANTOSH@192.168.1.79>
2023-08-21 15:44:10 +0530

    by santosh 2

--- Java/src/Java_Practice/Demo.java
+++ Java/src/Java_Practice/Demo.java
@@ -1,6 +1,13 @@
 package Java_Practice;
 
 public class Demo {
+	
+
+	public static void m1()
+	{
+		System.out.println("m1()method adde by satosh");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("changed by santosh");
